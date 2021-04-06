4fafce03304e59b0cae10ec5a434a96d92253067
marcelateo <marcelateodublin@gmail.com>
2021-04-06 10:36:07 +0000

    imageLogo

--- image-controller.js
+++ image-controller.js
@@ -17,4 +17,22 @@ exports.uploadImage = function(req, res){
         }
         res.status(201).send({newImage})
     });
-}
+
+};
+
+    exports.getImages = function(req, res) {
+    Image.find({}, '-__v')
+    .lean()
+    .exec((err, images) => {
+        if (err) {
+            return res.sendStatus(400);
+        }
+
+        for (let i = 0; i < images.length; i++) {
+            var img = images[i];
+            img.url = req.protocol + '://' + req.get('host') + '/images/' + img._id;
+        }
+
+        res.json(images);
+    });
+};
--- routes.js
+++ routes.js
@@ -20,4 +20,8 @@ var multer = require("multer");
 var upload = multer({dest: module.exports.UPLOAD_PATH});
 //define image controler
 var imageCtrl = require('./image-controller');
+
+router.post('/images', upload.single('image'), imageCtrl.uploadImage);
+router.get('/images', imageCtrl.getImages);
+
 module.exports = router;
